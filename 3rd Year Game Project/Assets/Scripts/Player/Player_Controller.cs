f6cf75dfa90c1fe1e516b9ebb369273137fcfec9
DAVV98 <danielvillatepsn@gmail.com>
2020-11-12 11:39:59 +0000

    Game

---
+++ 3rd Year Game Project/Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float horizontalInput;
+
+    [Header("Jumping")]
+    public bool isGrounded;
+    public Transform feetPos;
+    public float groundedRaycastLength;
+    public LayerMask groundLayer;
+    public float jumpForce;
+
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+
+
+    // Settings
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        // get this.objects rigidbody.
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Get horizontal imput.
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        Jumping();
+    }
+
+    private void FixedUpdate()
+    {
+        // call movement function
+        Movement();        
+    }
+
+    void Movement()
+    {
+        // change velocity to move player.
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+
+    void constantMovement(float input)
+    {
+        // makes float input into integer.
+        if (input < 0)
+        {
+            horizontalInput = -1;
+        }
+        else if (input > 0)
+        {
+            horizontalInput = 1;
+        }
+        else
+        {
+            horizontalInput = 0;
+        }
+    }
+
+    void Jumping()
+    {
+        // Check grounding
+        isGrounded = Physics2D.Raycast(feetPos.position, Vector2.down, groundedRaycastLength, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+
+        
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(feetPos.position, feetPos.position + Vector3.down * groundedRaycastLength);
+    }
+
+
+}
