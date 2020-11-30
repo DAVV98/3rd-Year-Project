28a4cb3482f874b1c92accbb3aeed2de7c622c30
DAVV98 <44123959+DAVV98@users.noreply.github.com>
2020-11-30 15:04:57 +0000

    Setting up PG

---
+++ 3rd Year Game Project/Assets/Chunk_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk_Controller : MonoBehaviour
+{
+    [Header("Row One")]
+    public GameObject chunk;
+    public int[] array;
+    public int chunk_seperation;
+
+    void Start()
+    {
+        load_chunks();
+    }
+
+    void load_chunks()
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+
+            Instantiate(chunk, new Vector3(i * chunk_seperation, this.transform.position.y, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Test_PF/Assets/Snake/Scripts/SwitchAIs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AlanZucconi.Snake;
+
+public class SwitchAIs : MonoBehaviour
+{
+
+    public SnakeGame Snake;
+
+    public SnakeAI AIA;
+    public SnakeAI AIB;
+
+    private bool first = true;
+
+    public KeyCode Key;
+
+	void Update ()
+    {
+		if (Input.GetKeyDown(Key))
+        {
+            Snake.AI = first ? AIB : AIA;
+            Snake.ReloadAI();
+
+            first = !first;
+        }
+	}
+}
